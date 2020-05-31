0e63589478c2ff3516d9dd2dfbd7bbaa10d948df
Manish Shridhar <manish.mait@gmail.com>
2020-06-01 04:58:30 +0530

    first commit

---
+++ src/test/java/com/manish/framework/sampleTest.java
@@ -0,0 +1,12 @@
+package com.manish.framework;
+
+import org.testng.annotations.Test;
+
+public class sampleTest {
+	
+	@Test
+	public void testIt() {
+		System.out.println("Test through Jenkins");
+	}
+
+}
